d0c0089c6b059f931750b9becd147084a844cd30
Fabiano Silva <fabianosilva.it@outlook.com>
2023-05-12 12:00:52 -0300

    Componente: SignInButton

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,14 +1,26 @@
-import { FaGithub } from 'react-icons/fa'
+import { FaGithub } from 'react-icons/fa';
+import { FiX } from 'react-icons/fi';
 
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-    return(
-        <button 
+
+    const isUserLoggedIn = true;
+
+    return isUserLoggedIn? (        
+            <button 
+                type="button"
+                className={styles.SignInbutton}>
+                <FaGithub color='#04d361'/>
+                Fabiano Silva
+                <FiX color='#737380' className={styles.closeIcons}/>
+            </button>
+        ) : (
+            <button 
             type="button"
             className={styles.SignInbutton}>
-            <FaGithub/>
+            <FaGithub color='#eba417'/>
             SignIn with Github
         </button>
-    )
+        );
 }
