c392d33e1c8bdeec32756458c6d61391425ca3bb
Fabiano Silva <fabianosilva.it@outlook.com>
2023-05-24 11:24:15 -0300

    Componente: SubscribeButton

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,15 @@
+import styles from './styles.module.scss';
+
+export function SubscribeButtoun(){
+    return(
+        (
+            <button
+                type="button"
+                className={styles.subscribeButton}>
+                    Subscribe now
+        
+            </button>
+         )   
+    )
+ 
+}
