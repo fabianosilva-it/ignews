16ded7c92730ad29752e60fc85702dee00e2d799
Fabiano Silva <fabianosilva.it@outlook.com>
2023-05-31 10:23:26 -0300

    Static Site Generation (SSG)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 
 import styles from './home.module.scss'
@@ -38,7 +38,7 @@ export default function Home({product}:HomeProps) {
   )
 }
 
-export const getServerSideProps:GetServerSideProps =async () => {
+export const getStaticProps:GetStaticProps =async () => {
   
   const price = await stripe.prices.retrieve('price_1NDT3fHW4dZ7pQCPPwKMekTI')
 
@@ -53,6 +53,7 @@ export const getServerSideProps:GetServerSideProps =async () => {
   return{
     props:{
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
